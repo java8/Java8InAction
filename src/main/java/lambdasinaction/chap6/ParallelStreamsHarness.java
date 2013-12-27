bed9477a729749a5021271132845666db40c10b7
Mario Fusco <mario.fusco@gmail.com>
2013-12-27 23:30:09 +0100

    Chapter 6

---
+++ src/main/java/lambdasinaction/chap6/ParallelStreamsHarness.java
@@ -0,0 +1,29 @@
+package lambdasinaction.chap6;
+
+import java.util.concurrent.*;
+import java.util.function.*;
+
+public class ParallelStreamsHarness {
+
+    public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
+
+    public static void main(String[] args) {
+        System.out.println("Sum done in: " + measurePerf(ParallelStreams::sum, 10_000_000L) + " msecs");
+        System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs" );
+        System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs");
+        System.out.println("Parallel range forkJoinSum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs" );
+        System.out.println("ForkJoin sum done in: " + measurePerf(ForkJoinSumCalculator::forkJoinSum, 10_000_000L) + " msecs" );
+    }
+
+    public static <T, R> long measurePerf(Function<T, R> f, T input) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            R result = f.apply(input);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Result: " + result);
+            if (duration < fastest) fastest = duration;
+        }
+        return fastest;
+    }
+}
---
+++ src/main/java/lambdasinaction/chap6/StreamForkerExample.java
@@ -0,0 +1,39 @@
+package lambdasinaction.chap6;
+
+import lambdasinaction.chap5.*;
+
+import static java.util.stream.Collectors.*;
+import static lambdasinaction.chap5.Dish.menu;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamForkerExample {
+
+    public static void main(String[] args) throws Exception {
+        processMenu();
+    }
+
+    private static void processMenu() {
+        Stream<Dish> menuStream = menu.stream();
+
+        StreamForker.Results results = new StreamForker<Dish>(menuStream)
+                .fork("shortMenu", s -> s.map(Dish::getName).collect(joining(", ")))
+                .fork("totalCalories", s -> s.mapToInt(Dish::getCalories).sum())
+                .fork("mostCaloricDish", s -> s.collect(
+                        reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2))
+                        .get())
+                .fork("dishesByType", s -> s.collect(groupingBy(Dish::getType)))
+                .getResults();
+
+        String shortMeny = results.get("shortMenu");
+        int totalCalories = results.get("totalCalories");
+        Dish mostCaloricDish = results.get("mostCaloricDish");
+        Map<Dish.Type, List<Dish>> dishesByType = results.get("dishesByType");
+
+        System.out.println("Short menu: " + shortMeny);
+        System.out.println("Total calories: " + totalCalories);
+        System.out.println("Most caloric dish: " + mostCaloricDish);
+        System.out.println("Dishes by type: " + dishesByType);
+    }
+}
