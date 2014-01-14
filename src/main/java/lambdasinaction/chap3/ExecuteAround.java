1ed225846a9e83ef94f7a70bab66865c70d2635b
raoulDoc <raoul@urma.com>
2014-01-14 15:28:16 +0000

    Adding limited version of processFile method

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -4,6 +4,13 @@ import java.io.*;
 public class ExecuteAround {
 
 	public static void main(String ...args) throws IOException{
+
+        // method we want to refactor to make more flexible
+        String result = processFileLimited();
+        System.out.println(result);
+
+        System.out.println("---");
+
 		String oneLine = processFile((BufferedReader b) -> b.readLine());
 		System.out.println(oneLine);
 
@@ -11,6 +18,14 @@ public class ExecuteAround {
 		System.out.println(twoLines);
 
 	}
+
+    public static String processFileLimited() throws IOException {
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+            return br.readLine();
+        }
+    }
+
 
 	public static String processFile(BufferedReaderProcessor p) throws IOException {
 		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
