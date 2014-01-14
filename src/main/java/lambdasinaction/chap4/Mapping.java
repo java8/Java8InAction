6ec8a0fd2af0c76357859ca29ac5893cd54031ed
raoulDoc <raoul@urma.com>
2014-01-14 13:21:34 +0000

    Added more code examples for chapter 4

--- src/main/java/lambdasinaction/chap4/Mapping.java
+++ src/main/java/lambdasinaction/chap4/Mapping.java
@@ -1,5 +1,5 @@
 package lambdasinaction.chap4;
-import java.util.stream.*;
+
 import java.util.*;
 import static java.util.stream.Collectors.toList;
 import static lambdasinaction.chap4.Dish.menu;
@@ -7,30 +7,30 @@ import static lambdasinaction.chap4.Dish.menu;
 public class Mapping{
 
     public static void main(String...args){
-    
+
         // map
         List<String> dishNames = menu.stream()
                                      .map(Dish::getName)
                                      .collect(toList());
         System.out.println(dishNames);
-        
+
         // map
         List<String> words = Arrays.asList("Hello", "World");
         List<Integer> wordLengths = words.stream()
                                          .map(String::length)
                                          .collect(toList());
         System.out.println(wordLengths);
-        
-        // flatMap                 
+
+        // flatMap
         words.stream()
                  .flatMap((String line) -> Arrays.stream(line.split("")))
                  .distinct()
                  .forEach(System.out::println);
-               
+
         // flatMap
         List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
         List<Integer> numbers2 = Arrays.asList(6,7,8);
-        List<int[]> pairs = 
+        List<int[]> pairs =
                         numbers1.stream()
                                 .flatMap(i -> numbers2.stream()
                                                        .map(j -> new int[]{i, j})
--- src/main/java/lambdasinaction/chap4/StreamBasic.java
+++ src/main/java/lambdasinaction/chap4/StreamBasic.java
@@ -2,6 +2,8 @@ package lambdasinaction.chap4;
 
 import java.util.*;
 import java.util.stream.*;
+
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 import static lambdasinaction.chap4.Dish.menu;
@@ -9,11 +11,40 @@ import static lambdasinaction.chap4.Dish.menu;
 public class StreamBasic {
 
     public static void main(String...args){
-        List<String> names = menu.stream()
-                                 .filter(d -> d.getCalories() > 300)
-                                 .map(Dish::getName)
-                                 .limit(3)
-                                 .collect(toList());              
-        System.out.println(names);
+        // Java 7
+        getLowCaloricDishesNamesInJava7(Dish.menu).forEach(System.out::println);
+
+        System.out.println("---");
+
+        // Java 8
+        getLowCaloricDishesNamesInJava8(Dish.menu).forEach(System.out::println);
+
+    }
+
+    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes){
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for(Dish d: dishes){
+            if(d.getCalories() > 400){
+                lowCaloricDishes.add(d);
+            }
+        }
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            public int compare(Dish d1, Dish d2){
+                return Integer.compare(d1.getCalories(), d2.getCalories());
+            }
+        });
+        for(Dish d: lowCaloricDishes){
+            lowCaloricDishesName.add(d.getName());
+        }
+        return lowCaloricDishesName;
+    }
+
+    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes){
+        return dishes.stream()
+                .filter(d -> d.getCalories() > 400)
+                .sorted(comparing(Dish::getCalories))
+                .map(Dish::getName)
+                .collect(toList());
     }
 }
