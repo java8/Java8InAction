150e8bd01b635809888dc8345527d4f4c98167b9
Mario Fusco <mario.fusco@gmail.com>
2014-03-01 14:51:19 +0100

    add Optional examples

---
+++ src/main/java/lambdasinaction/chap9/Insurance.java
@@ -0,0 +1,10 @@
+package lambdasinaction.chap9;
+
+public class Insurance {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/lambdasinaction/chap9/Person.java
@@ -0,0 +1,12 @@
+package lambdasinaction.chap9;
+
+import java.util.*;
+
+public class Person {
+
+    private Optional<Car> car;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
