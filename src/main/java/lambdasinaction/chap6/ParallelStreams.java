b2338a46e2c6f445066fac833c45fb1d4906df27
Mario Fusco <mario.fusco@gmail.com>
2014-05-07 12:07:35 +0200

    fix parallel stream harness

--- src/main/java/lambdasinaction/chap6/ParallelStreams.java
+++ src/main/java/lambdasinaction/chap6/ParallelStreams.java
@@ -6,7 +6,7 @@ public class ParallelStreams {
 
     public static long iterativeSum(long n) {
         long result = 0;
-        for (long i = 0; i < n; i++) {
+        for (long i = 0; i <= n; i++) {
             result += i;
         }
         return result;
--- src/main/java/lambdasinaction/chap6/ParallelStreamsHarness.java
+++ src/main/java/lambdasinaction/chap6/ParallelStreamsHarness.java
@@ -8,7 +8,7 @@ public class ParallelStreamsHarness {
     public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
 
     public static void main(String[] args) {
-        System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
+        System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
         System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
         System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs" );
         System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs");
