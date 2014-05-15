3e7d2dd1328a9653054d753bca250cb33b37bf73
raoulDoc <raoul@urma.com>
2014-05-15 23:30:24 +0100

    added clarifications for code

--- src/main/java/lambdasinaction/chap4/StreamVsCollection.java
+++ src/main/java/lambdasinaction/chap4/StreamVsCollection.java
@@ -11,6 +11,8 @@ public class StreamVsCollection {
         List<String> names = Arrays.asList("Java8", "Lambdas", "In", "Action");
         Stream<String> s = names.stream();
         s.forEach(System.out::println);
-        s.forEach(System.out::println); // Exception 
+        // uncommenting this line will result in an IllegalStateException
+        // because streams can be consumed only once
+        //s.forEach(System.out::println);
     }
 }
