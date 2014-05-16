b3c9000bc4748d39c4e562d283ae3015c329f129
raoulDoc <raoul@urma.com>
2014-05-16 17:08:37 +0100

    chapter 7 updates

---
+++ src/main/java/lambdasinaction/chap7/Debugging.java
@@ -0,0 +1,32 @@
+package lambdasinaction.chap7;
+
+/**
+ * Created by raoul-gabrielurma on 16/05/2014.
+ */
+import java.util.*;
+
+public class Debugging{
+    public static void main(String[] args) {
+        List<Point> points = Arrays.asList(new Point(12, 2), null);
+        points.stream().map(p -> p.getX()).forEach(System.out::println);
+    }
+
+
+    private static class Point{
+        private int x;
+        private int y;
+
+        private Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+    }
+}
---
+++ src/main/java/lambdasinaction/chap7/Peek.java
@@ -0,0 +1,21 @@
+package lambdasinaction.chap7;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by raoul-gabrielurma on 16/05/2014.
+ */
+public class Peek {
+
+    public static void main(String[] args) {
+
+        List<Integer> result = Stream.of(2, 3, 4, 5)
+                .peek(x -> System.out.println("taking from stream: " + x)).map(x -> x + 17)
+                .peek(x -> System.out.println("after map: " + x)).filter(x -> x % 2 == 0)
+                .peek(x -> System.out.println("after filter: " + x)).limit(3)
+                .peek(x -> System.out.println("after limit: " + x)).collect(toList());
+    }
+}
