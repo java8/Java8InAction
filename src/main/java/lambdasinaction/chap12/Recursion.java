ed03fc18ff8e34d99102bff1c3327925e6b34533
raoulDoc <raoul@urma.com>
2014-05-16 17:21:28 +0100

    chapter 12 updates

---
+++ src/main/java/lambdasinaction/chap12/Recursion.java
@@ -0,0 +1,41 @@
+package lambdasinaction.chap12;
+
+import java.util.stream.LongStream;
+
+/**
+ * Created by raoul-gabrielurma on 16/05/2014.
+ */
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println(factorialIterative(5));
+        System.out.println(factorialRecursive(5));
+        System.out.println(factorialStreams(5));
+        System.out.println(factorialTailRecursive(5));
+    }
+
+    public static int factorialIterative(int n) {
+        int r = 1;
+        for (int i = 1; i <= n; i++) {
+            r*=i;
+        }
+        return r;
+    }
+
+    public static long factorialRecursive(long n) {
+        return n == 1 ? 1 : n*factorialRecursive(n-1);
+    }
+
+    public static long factorialStreams(long n){
+        return LongStream.rangeClosed(1, n)
+                         .reduce(1, (long a, long b) -> a * b);
+    }
+
+    public static long factorialTailRecursive(long n) {
+        return factorialHelper(1, n);
+    }
+
+    public static long factorialHelper(long acc, long n) {
+        return n == 1 ? acc : factorialHelper(acc * n, n-1);
+    }
+}
---
+++ src/main/java/lambdasinaction/chap12/SubsetsMain.java
@@ -0,0 +1,48 @@
+package lambdasinaction.chap12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by raoul-gabrielurma on 16/05/2014.
+ */
+public class SubsetsMain {
+
+    public static void main(String[] args) {
+        List<List<Integer>> subs = subsets(Arrays.asList(1, 4, 9));
+
+        subs.forEach(System.out::println);
+
+    }
+
+
+    public static List<List<Integer>> subsets(List<Integer> l) {
+        if (l.isEmpty()) {
+            List<List<Integer>> ans = new ArrayList<>();
+            ans.add(Collections.emptyList());
+            return ans;
+        }
+        Integer first = l.get(0);
+        List<Integer> rest = l.subList(1,l.size());
+        List<List<Integer>> subans = subsets(rest);
+        List<List<Integer>> subans2 = insertAll(first, subans);
+        return concat(subans, subans2);
+    }
+
+    public static List<List<Integer>> insertAll(Integer first, List<List<Integer>> lists) {
+        List<List<Integer>> result = new ArrayList<>(); for (List<Integer> l : lists) {
+            List<Integer> copyList = new ArrayList<>(l);
+            copyList.add(first);
+            result.add(copyList);
+        }
+        return result;
+    }
+
+    static List<List<Integer>> concat(List<List<Integer>> a, List<List<Integer>> b) {
+        List<List<Integer>> r = new ArrayList<>(a);
+        r.addAll(b);
+        return r;
+    }
+}
