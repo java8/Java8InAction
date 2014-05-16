dc888bc2a842a6e4e21e771314c78130141d0e6b
raoulDoc <raoul@urma.com>
2014-05-16 19:17:29 +0100

    combinators chap 13

--- src/main/java/lambdasinaction/chap13/Combinators.java
+++ src/main/java/lambdasinaction/chap13/Combinators.java
@@ -1,5 +1,20 @@
 package lambdasinaction.chap13;
 
 
+import java.util.function.Function;
+
 public class Combinators {
+
+    public static void main(String[] args) {
+        System.out.println(repeat(3, (Integer x)->2*x).apply(10));
+    }
+
+    static <A,B,C> Function<A,C> compose(Function<B,C> g, Function<A,B> f) {
+        return x -> g.apply(f.apply(x));
+    }
+
+    static <A> Function<A,A> repeat(int n, Function<A,A> f) {
+        return n==0?x->x : compose(f, (Function<A,A>) repeat(n-1, f));
+    }
+
 }
