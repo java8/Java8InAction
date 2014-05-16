8b98c74cc5a8e34567e5003a21c50ccecd335c45
raoulDoc <raoul@urma.com>
2014-05-16 17:52:13 +0100

    updates chapter 13

---
+++ src/main/java/lambdasinaction/chap13/Currying.java
@@ -0,0 +1,28 @@
+package lambdasinaction.chap13;
+
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Created by raoul-gabrielurma on 16/05/2014.
+ */
+public class Currying {
+
+    public static void main(String[] args) {
+        DoubleUnaryOperator convertCtoF = curriedConverter(9.0/5, 32);
+        DoubleUnaryOperator convertUSDtoGBP = curriedConverter(0.6, 0);
+        DoubleUnaryOperator convertKmtoMi = curriedConverter(0.6214, 0);
+
+        System.out.println(convertCtoF.applyAsDouble(24));
+        System.out.println(convertUSDtoGBP.applyAsDouble(100));
+        System.out.println(convertKmtoMi.applyAsDouble(20));
+    }
+
+    static double converter(double x, double y, double z) {
+        return x * y + z;
+    }
+
+    static DoubleUnaryOperator curriedConverter(double y, double z) {
+        return (double x) -> x * y + z;
+    }
+
+}
