58ba0e88eb2b744ac92169ebf37410671e6a0bed
Russell-JX <jxoucjj@126.com>
2021-08-29 14:53:06 +0800

    test

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 
 public class FilteringApples{
 
-    public static void main(String ... args){
+    public static void main(String ... args){ 
 
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
